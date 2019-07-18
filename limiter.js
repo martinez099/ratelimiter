7d48e5dc481e32175bae2da85eb8ebef3d440cfd
Martin Forstner <martin@redislabs.com>
2019-07-18 11:19:54 +0200

    optimize a bit

--- limiter.js
+++ limiter.js
@@ -6,17 +6,18 @@ const PER_MS = 1000;
 
 module.exports = function(redis, cb) {
 
+  var now = Date.now()
+
   // record current request
-  redis.zadd(KEY_NAME, Date.now(), uuid());
+  redis.zadd(KEY_NAME, now, uuid());
 
   // count number of requests in the last PER_MS milliseconds
-  redis.zcount(KEY_NAME, Date.now() - PER_MS, Date.now(), function(error, result) {
+  redis.zcount(KEY_NAME, now - PER_MS, now, function(error, result) {
     if (error) {
       cb(error);
     } else {
-
       // remove outdated requests, i.e. older than PER_MS milliseconds
-      redis.zremrangebyscore(KEY_NAME, 0, Date.now() - PER_MS);
+      redis.zremrangebyscore(KEY_NAME, 0, now - PER_MS);
 
       // deny access if more than MAX_REQ requests
       if (result > MAX_REQ) {
